0abcae158c7e91501f7e7497b9d41e87b080c786
saad-alabyad <saadalabyad78@gmail.com>
2026-01-21 01:51:34 +0300

    auto scan

---
+++ Models/AutoScanResult.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Plustek.Models {
+    public class AutoScanResult {
+        public string FileName { get; set; } = "";
+        public string? Base64 { get; set; }
+        public string? OcrText { get; set; }
+        public DateTime ScannedAt { get; set; } = DateTime.Now;
+    }
+}
---
+++ Models/PaperStatus.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Plustek.Models {
+    public class PaperStatus {
+        public int EventCode { get; set; }
+        public string Message { get; set; } = "";
+
+        public bool IsPaperPresent => EventCode == 0 || EventCode == 11 || EventCode == 12 || EventCode == 13;
+        public bool IsNoPaper => EventCode == 1;
+    }
+}
